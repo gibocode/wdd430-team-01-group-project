60698cf20080621984d7045435aa919f9da9c251
Gilbor Camporazo Jr. <gilborcamporazojr@programmer.net>
2026-03-25 02:03:42 +0800

    Implement Review API to get all product reviews, create product review, and delete product review.

---
+++ app/api/products/[productId]/reviews/route.ts
@@ -0,0 +1,73 @@
+import { getAllReviewsByProductId, createReview } from "@/models/review";
+import { z } from 'zod';
+import { Review } from '@/types/review';
+import { ObjectId } from 'mongodb';
+
+// Product review data validation
+const ReviewSchema = z.object({
+    rating: z.number(),
+    reviewer: z.string(),
+    comment: z.string().min(5)
+});
+
+// API to get all product reviews
+export async function GET(request: Request, { params }: { params : Promise<{ productId: string }> }): Promise<Response> {
+    try {
+        const { productId } = await params;
+
+        // Get all product reviews
+        const reviews = await getAllReviewsByProductId(productId);
+
+        return Response.json({
+            success: true,
+            data: reviews
+        });
+    }
+    catch (error) {
+        console.error(error);
+        return Response.json(
+            {
+                success: false,
+                message: 'Failed to fetch product reviews.',
+            },
+            { status: 500 }
+        );
+    }
+}
+
+// API to create a product review
+export async function POST(request: Request, { params }: { params: Promise<{ productId: string }>}): Promise<Response> {
+    try {
+        const { productId } = await params;
+        const body = await request.json();
+
+        // Validates product review data
+        const validated = ReviewSchema.parse(body);
+
+        const data: Review = {
+            ...validated,
+            productId: new ObjectId(productId),
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        }
+
+        // Creates product review
+        const review = await createReview(data);
+
+        return Response.json({
+            success: true,
+            message: 'Product review successfully created.',
+            data: review
+        });
+    }
+    catch (error) {
+        console.error(error);
+        return Response.json(
+            {
+                success: false,
+                message: 'Failed to create a product review.',
+            },
+            { status: 500 }
+        );
+    }
+}
